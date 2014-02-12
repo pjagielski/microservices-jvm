c9bfbfe0acdf95ff218fee46ad4dd1103b16408c
Piotr Jagielski <pjg@touk.pl>
2014-02-12 22:43:07 +0100

    lein new

---
+++ compojure-rest/test/compojure_rest/test/handler.clj
@@ -0,0 +1,14 @@
+(ns compojure-rest.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        compojure-rest.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
