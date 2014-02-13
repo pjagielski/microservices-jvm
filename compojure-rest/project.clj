aabde973cf7415c4efe11a9c76b1ebb5d94fd8f5
Piotr Jagielski <pjg@touk.pl>
2014-02-13 22:45:15 +0100

    uberjar

--- compojure-rest/project.clj
+++ compojure-rest/project.clj
@@ -3,9 +3,11 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
+                 [ring/ring-jetty-adapter "1.2.0"]
                  [ring-middleware-format "0.3.0"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler compojure-rest.handler/app}
+  :main compojure-rest.handler
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
--- compojure-rest/src/compojure_rest/handler.clj
+++ compojure-rest/src/compojure_rest/handler.clj
@@ -2,7 +2,9 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.middleware.format :refer :all]))
+            [ring.middleware.format :refer :all]
+            [ring.adapter.jetty :refer (run-jetty)])
+  (:gen-class))
 
 (defroutes app-routes
   (GET "/messages/:name" [name] {:body {:message (str "Hello World" " " name)}})
@@ -12,3 +14,6 @@
 (def app
   (-> (handler/api app-routes)
       (wrap-restful-format :formats [:json-kw :edn])))
+
+(defn -main [& args]
+  (run-jetty app {:port 3000 :join? false }))
