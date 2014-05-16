5c49ca68291268c87e93471daf970bea6a235267
Piotr Jagielski <pjg@touk.pl>
2014-05-16 17:47:45 +0200

    compojure - post example

--- compojure-rest/project.clj
+++ compojure-rest/project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
                  [ring/ring-jetty-adapter "1.2.0"]
-                 [ring-middleware-format "0.3.0"]]
+                 [ring/ring-core "1.2.0"]
+                 [ring/ring-json "0.1.2"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler compojure-rest.handler/app}
   :main compojure-rest.handler
--- compojure-rest/src/compojure_rest/handler.clj
+++ compojure-rest/src/compojure_rest/handler.clj
@@ -2,18 +2,35 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.middleware.format :refer :all]
+            [ring.middleware.json :as middleware]
+            [ring.util.response :refer [resource-response response status]]
             [ring.adapter.jetty :refer (run-jetty)])
   (:gen-class))
 
+(def messages (atom {}))
+
+(defn get-message [name]
+  (get (deref messages) name))
+
+(defn put-message [name message]
+  (swap! messages assoc name message))
+
 (defroutes app-routes
-  (GET "/messages/:name" [name] {:body {:message (str "Hello World" " " name)}})
+  (GET "/messages/:name" [name]
+       (response (get-message name)))
+
+  (POST "/messages" {message :body}
+        (let [name (:name message)]
+          (put-message name message)
+          (status (response "") 204)))
+
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
   (-> (handler/api app-routes)
-      (wrap-restful-format :formats [:json-kw :edn])))
+      (middleware/wrap-json-body {:keywords? true})
+      (middleware/wrap-json-response)))
 
 (defn -main [& args]
   (run-jetty app {:port 3000 :join? false }))
